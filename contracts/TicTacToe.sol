20ae7d847c1f597788b768b9413bc6e206f6d018
Gideon Spierings <gideongideon@gmail.com>
2022-02-05 12:44:21 +0100

    Backup before refactor

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -11,6 +11,7 @@ contract TicTacToe is Ownable {
     event MoveMade(uint256 gameId, address player, uint8 _cell);
 
     struct TicTacToeGame {
+        uint256 id;
         uint8[9] board;
         address player1;
         address player2;
@@ -27,6 +28,9 @@ contract TicTacToe is Ownable {
     function getBoard() external view returns (uint8[9] memory) {
         require(_hasActiveGame(msg.sender), "Error: You don't have an active game.");
         uint256 gameId = playerActiveGame[msg.sender];
+
+        console.log("_hasActiveGame", _hasActiveGame(msg.sender));
+
         return games[gameId].board;
     }
 
@@ -48,6 +52,10 @@ contract TicTacToe is Ownable {
         }
 
         return openGames;
+    }
+
+    function hasActiveGame() external view returns (bool) {
+        return _hasActiveGame(msg.sender);
     }
 
     function isPlayer1(uint256 _gameId, address _player) public view returns (bool) {
@@ -128,6 +136,7 @@ contract TicTacToe is Ownable {
 
         games.push(
             TicTacToeGame({
+                id: games.length,
                 board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 player1: msg.sender,
                 player2: address(0),
@@ -152,17 +161,25 @@ contract TicTacToe is Ownable {
 
         games[_gameId].player2 = msg.sender;
 
+        console.log("join succesful:");
+        console.log(games[_gameId].player1);
+        console.log(games[_gameId].player2);
+        console.log(games[_gameId].winner);
+        console.log(games[_gameId].turn);
+        console.log(games[_gameId].buyIn);
+
         _setActiveGame(msg.sender, _gameId);
     }
 
-    function makeMove(uint256 _gameId, uint8 _cell) external {
-        require(_gameId < games.length, "Error: Game does not exist.");
+    function makeMove(uint8 _cell) external {
+        require(_hasActiveGame(msg.sender), "Error: You don't have an active game.");
 
-        TicTacToeGame memory game = games[_gameId];
+        uint256 gameId = _getActiveGame(msg.sender);
+        TicTacToeGame memory game = games[gameId];
 
         require(game.player2 != address(0), "Error: This game has not started yet.");
         require(
-            isPlayer1(_gameId, msg.sender) || isPlayer2(_gameId, msg.sender),
+            isPlayer1(gameId, msg.sender) || isPlayer2(gameId, msg.sender),
             "Error: You are not playing in this game."
         );
         require(game.turn == msg.sender, "Error: It is not your turn.");
@@ -171,16 +188,32 @@ contract TicTacToe is Ownable {
         require(game.board[_cell] == 0, "Error: This cell is already occupied.");
 
         game.board[_cell] = msg.sender == game.player1 ? 1 : 2;
-        emit MoveMade(_gameId, msg.sender, _cell);
+        emit MoveMade(gameId, msg.sender, _cell);
 
-        if (_hasWonGame(_gameId)) {
+        if (_hasWonGame(gameId)) {
             game.winner = msg.sender;
             game.turn = address(0);
-            emit GameEnded(_gameId, game.player1, game.player2, game.winner);
+            emit GameEnded(gameId, game.player1, game.player2, game.winner);
             _unsetActiveGame(game.player1);
             _unsetActiveGame(game.player2);
         } else {
             game.turn = msg.sender == game.player1 ? game.player2 : game.player1;
         }
+
+        console.log("move succesful:");
+        console.log("cell0: ", game.board[0]);
+        console.log("cell1: ", game.board[1]);
+        console.log("cell2: ", game.board[2]);
+        console.log("cell3: ", game.board[3]);
+        console.log("cell4: ", game.board[4]);
+        console.log("cell5: ", game.board[5]);
+        console.log("cell6: ", game.board[6]);
+        console.log("cell7: ", game.board[7]);
+        console.log("cell8: ", game.board[8]);
+        console.log("player1: ", game.player1);
+        console.log("player2: ", game.player2);
+        console.log("winner: ", game.winner);
+        console.log("turn: ", game.turn);
+        console.log("buyIn: ", game.buyIn);
     }
 }
